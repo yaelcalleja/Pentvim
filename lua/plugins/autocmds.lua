af47ef92d58d1932f64bccf64a421520d08cba67
Badger <yael.calleja321@gmail.com>
2025-10-16 19:42:08 -0600

    Nvim just tailor-made for me

--- lua/plugins/autocmds.lua
+++ lua/plugins/autocmds.lua
@@ -10,7 +10,7 @@ autocmd('FileType', {
   callback = function()
     -- :w guarda el archivo antes de ejecutar
     -- 'read -p' mantiene la terminal abierta hasta que presiones Enter
-    vim.keymap.set('n', '<leader>r', ':w | belowright 12split | terminal g++ % -o %< && ./%< && echo "" && read -p "Presiona Enter para cerrar..." && exit<CR> | startinsert',
+    vim.keymap.set('n', '<leader>r', ':w | belowright 12split | terminal g++ % -o %< && ./%< && echo "" && read -p "Presiona Enter para cerrar..." && exit<CR>',
       { buffer = true, noremap = true, silent = true, desc = "Compilar y ejecutar C++" })
   end,
 })
@@ -26,7 +26,7 @@ autocmd('FileType', {
   group = augroup,
   pattern = 'python',
   callback = function()
-    vim.keymap.set('n', '<leader>r', ':w | belowright 12split | terminal python3 %<CR> | startinsert',
+    vim.keymap.set('n', '<leader>r', ':w | belowright 12split | terminal python3 %<CR>',
       { buffer = true, noremap = true, silent = true, desc = "Ejecutar Python" })
   end,
 })
