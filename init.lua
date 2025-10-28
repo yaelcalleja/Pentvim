a345d4c1b7fb7d026ba28e3ba33fc4f2370491a3
Badger <yael.calleja321@gmail.com>
2025-10-28 07:33:43 -0600

    Main file of the config, all loaded plugins are in here.

--- init.lua
+++ init.lua
@@ -9,7 +9,6 @@ require('badger.diagnostics')
 --Load the conf for the plugins
 require('plugins.lsp')
 require('plugins.cmp')
---require('plugins.null-ls')
 require('plugins.lualine')
 require('plugins.treesitter')
 require('plugins.telescope')
